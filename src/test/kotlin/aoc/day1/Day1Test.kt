26da9e2020a9c9dd18b540e8d3826364ea5bce20
vdsalex <alexandre.vandensteen@gmail.com>
2023-12-01 17:05:05 +0100

    day 1 part 2

--- src/test/kotlin/aoc/day1/Day1Test.kt
+++ src/test/kotlin/aoc/day1/Day1Test.kt
@@ -2,13 +2,16 @@ package aoc.day1
 
 import aoc.Utils.filePath
 import aoc.Utils.linesFromFile
+import aoc.model.Number
+import aoc.model.Number.Companion.formatted
+import aoc.model.Number.Companion.toDigitValue
 import org.junit.jupiter.api.Test
 
 class Day1Test {
 
     @Test
     fun `should extract calibration values from document`() {
-        val lines = linesFromFile(filePath("/01/01-input")) // test
+        val lines = linesFromFile(filePath("/01/01-input"))
 
         val total = lines.map { line ->
             line.toCharArray()
@@ -24,7 +27,40 @@ class Day1Test {
     }
 
     @Test
-    fun `should do something`() {
-        TODO("Not yet implemented")
+    fun `should extract calibration values from document even if values are in character`() {
+        val lines = linesFromFile(filePath("/01/01-input"))
+        val updatedLines: MutableList<String> = mutableListOf()
+
+        lines.forEach { line ->
+            var temp = ""
+            var tempLine = ""
+            line.toCharArray().map { c ->
+                if (c.isDigit()) {
+                    tempLine += c
+                    temp = ""
+                } else {
+                    temp += c
+                    val foundNumber = Number.values().firstOrNull { it.formatted() in temp }
+
+                    if (foundNumber != null) {
+                        tempLine += foundNumber.toDigitValue()
+                        temp = c.toString()
+                    }
+                }
+            }
+            updatedLines.add(tempLine)
+        }
+
+        val total = updatedLines.map { line ->
+            line.toCharArray()
+                .filter { c -> c.isDigit()  }
+                .joinToString(separator = "")
+        }.sumOf {
+            listOf(it.first(), it.last())
+                .joinToString(separator = "")
+                .toInt()
+        }
+
+        println("total: $total")
     }
 }
