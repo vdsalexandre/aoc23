4eaa462a21d9d438362ab3063ecb39f1e4fc0b44
vdsalex <alexandre.vandensteen@gmail.com>
2023-12-02 18:06:26 +0100

    prepare day 3

---
+++ src/test/kotlin/aoc/day/Day3Test.kt
@@ -0,0 +1,21 @@
+package aoc.day
+
+import aoc.Utils
+import org.junit.jupiter.api.Test
+
+class Day3Test {
+
+    @Test
+    fun `day 3 part 1`() {
+        val lines = Utils.linesFromFile(Utils.filePath("/03/test1"))
+
+        lines.forEach(::println)
+    }
+
+    @Test
+    fun `day 3 part 2`() {
+        val lines = Utils.linesFromFile(Utils.filePath("/03/test1"))
+
+        lines.forEach(::println)
+    }
+}
