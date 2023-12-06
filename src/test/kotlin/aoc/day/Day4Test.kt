1a32826839428e891ea49ac1dd60589d6618e9f9
vdsalex <alexandre.vandensteen@gmail.com>
2023-12-06 23:58:15 +0100

    day 4 part 2

--- src/test/kotlin/aoc/day/Day4Test.kt
+++ src/test/kotlin/aoc/day/Day4Test.kt
@@ -7,11 +7,14 @@ import org.junit.jupiter.api.Test
 
 class Day4Test {
 
-    private val points = listOf(0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16_384, 32_768, 65_536, 131_072)
+    private val points =
+        listOf(0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16_384, 32_768, 65_536, 131_072)
+    private val allCommonNumbers: MutableList<NumberLine> = mutableListOf()
+    private var count = 0
 
     @Test
     fun `day 4 part 1`() {
-        val lines = Utils.linesFromFile(Utils.filePath("/04/input"))
+        val lines = Utils.linesFromFile(Utils.filePath("/04/test"))
         val winningNumbers: MutableList<NumberLine> = mutableListOf()
         val playedNumbers: MutableList<NumberLine> = mutableListOf()
 
@@ -33,7 +36,7 @@ class Day4Test {
 
     @Test
     fun `day 4 part 2`() {
-        val lines = Utils.linesFromFile(Utils.filePath("/04/test"))
+        val lines = Utils.linesFromFile(Utils.filePath("/04/input"))
         val winningNumbers: MutableList<NumberLine> = mutableListOf()
         val playedNumbers: MutableList<NumberLine> = mutableListOf()
 
@@ -44,11 +47,33 @@ class Day4Test {
             playedNumbers.add(NumberLine(stringNumbersToList(part[1]), cardId))
         }
 
-        val allCommonNumbers: MutableList<NumberLine> = mutableListOf()
         for (i in 0 until winningNumbers.size) {
-            allCommonNumbers.add(NumberLine(winningNumbers[i].getCommonNumbers(playedNumbers[i].numbers), winningNumbers[i].cardId))
+            allCommonNumbers.add(
+                NumberLine(
+                    numbers = winningNumbers[i].getCommonNumbers(playedNumbers[i].numbers),
+                    cardId = winningNumbers[i].cardId
+                )
+            )
         }
 
-        allCommonNumbers.forEach(::println)
+        allCommonNumbers.forEach { game ->
+//            allScratchCards.add(game)
+            count++
+            findNextScratchCards(game)
+        }
+
+//        allScratchCards.sortedBy { it.cardId }.forEach(::println)
+
+        println(count)
+    }
+
+    private fun findNextScratchCards(game: NumberLine) {
+        for (i in 1..game.numbers.size) {
+            val newScratchCard = allCommonNumbers.first { it.cardId == game.cardId + i }
+            count++
+            if (newScratchCard.numbers.isNotEmpty()) {
+                findNextScratchCards(newScratchCard)
+            }
+        }
     }
 }
